ac2a551c7fed656fb066b47bb486a1e5a7bc54e4
Ferry To <ferrywlto@gmail.com>
2025-07-06 19:19:53 +0100

    chore: Remove unused doc and update site url to personal site.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://verdantsparks.io',
+  site: 'https://ferrywl.to',
   base: 'theme-color-playground',
   output: 'static'
 });
